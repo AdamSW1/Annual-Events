9baae0cd0c1a021a09f70c86c27f4ac58efd4fee
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-03-15 17:05:33 -0400

    my search cs methods

--- Annual-events/BusinessLayer/Search.cs
+++ Annual-events/BusinessLayer/Search.cs
@@ -1,5 +1,47 @@
 namespace BusinessLayer;
 
 class Search {
-    
+    private string _keywords;
+    public string Keywords { get; set; }
+
+    public Search(string keywords)
+    {
+        Keywords = keywords;
+    }
+
+    public void SortByIngredients()
+    {
+        // Sort recipes by ingredients
+        throw new NotImplementedException();
+    }
+    public void SortByRatings()
+    {
+        // Sort recipes by ratings
+        throw new NotImplementedException();
+    }
+    public  void SortByCookingTime()
+    {
+        // Sort recipes by cooking time
+        throw new NotImplementedException();
+    }
+    public void SortByKeyword()
+    {
+        // Sort recipes by keyword
+        throw new NotImplementedException();
+    }
+    public void SortByRecipeInFavorites()
+    {
+        // Sort recipes by favorites
+        throw new NotImplementedException();
+    }
+    public void SortByServings()
+    {
+        // Sort recipes by servings
+        throw new NotImplementedException();
+    }
+    public void SortByOwnership()
+    {
+        // Sort recipes by ownership
+        throw new NotImplementedException();
+    }
 }
