c34c91c700d7663ad4f3416ad8c12e21db40a862
Elias Dif <elias.dif@dawsoncollege.qc.ca>
2024-03-17 19:58:37 -0400

    Updated repo and added ComparatorsTest which tests all of the Comparators that we'll use

---
+++ annual-events-test/ComparatorsTest.cs
@@ -0,0 +1,33 @@
+namespace annual_events_test;
+
+[TestClass]
+public class ComparatorsTest
+{
+    // Tests CompareByIngredient
+    [TestMethod]
+    public void TestIngredientsComparator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Tests CompareByRating
+    [TestMethod]
+    public void TestRatingComparator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Tests CompareByServing
+    [TestMethod]
+    public void TestServingsComparator()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Tests CompareByTime
+    [TestMethod]
+    public void TestTimeComparator()
+    {
+        throw new NotImplementedException();
+    }
+}
