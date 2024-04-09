3a97f409121240c19c1dea4d340348337dd1be5c
AdamSW1 <adamseanwinter@outlook.com>
2024-04-09 16:45:41 -0400

    implemented creating a user in the program POC

--- Annual-events/BusinessLayer/Program.cs
+++ Annual-events/BusinessLayer/Program.cs
@@ -1,9 +1,21 @@
 using System;
-
+using BusinessLayer;
 class Program
 {
     static void Main(string[] args)
     {
-        // main program to run
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter a password:");
+        string password = Console.ReadLine();
+        Console.WriteLine("Enter description:");
+        string description = Console.ReadLine();
+        Console.WriteLine("Enter age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        User newUser = new User(username, password, description, age);
+
+        
+
+
     }
 }
