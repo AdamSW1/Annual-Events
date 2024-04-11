19f4aec4fd1114928370b19c5f8c456cd382f1ec
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-04-11 16:12:31 -0400

    Added utils class for utilities method

---
+++ Annual-events/BusinessLayer/Utils.cs
@@ -0,0 +1,17 @@
+using BusinessLayer;
+using RecipeInfo;
+using Microsoft.EntityFrameworkCore;
+namespace BusinessLayer;
+
+class Utils
+{
+    public List<RecipeTags> ValidateTags(List<string> tags)
+    {
+        //Make a list of all enums in recipeTags
+        var enum_tags = Enum.GetValues(typeof(RecipeTags));
+        List<string> string_enum_tags = enum_tags.Cast<string>().ToList();
+        //Check if the input tags matches any of the available tags in the enums and adds it to a list
+        var available_tags = string_enum_tags.Where(tag => tags.Contains(tag));
+        return available_tags.Cast<RecipeTags>().ToList();
+    }
+}
