53e78a9685a9ad88f463f41c17de61ca8bcd60b8
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-04-16 14:38:05 -0400

    Added and fixed Validate tag method

--- Annual-events/BusinessLayer/Utils.cs
+++ Annual-events/BusinessLayer/Utils.cs
@@ -9,9 +9,9 @@ class Utils
     {
         //Make a list of all enums in recipeTags
         var enum_tags = Enum.GetValues(typeof(RecipeTags));
-        List<string> string_enum_tags = enum_tags.Cast<string>().ToList();
+        List<string> string_enum_tags = Array.ConvertAll((RecipeTags[])enum_tags, item => item.ToString()).ToList();
         //Check if the input tags matches any of the available tags in the enums and adds it to a list
-        var available_tags = string_enum_tags.Where(tag => tags.Contains(tag));
-        return available_tags.Cast<RecipeTags>().ToList();
+        var available_tags = string_enum_tags.Where(tag => tags.Contains(tag)).ToList();
+        return Array.ConvertAll(available_tags.ToArray(), item => (RecipeTags)Enum.Parse(typeof(RecipeTags), item)).ToList();
     }
 }
