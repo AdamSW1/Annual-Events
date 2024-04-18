7f17fcc165a2395d126070842ff4669556d10380
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-04-17 22:37:54 -0400

    Added search tests for tags

--- annual-events-test/Search-Test.cs
+++ annual-events-test/Search-Test.cs
@@ -46,4 +46,207 @@ public class SearchTest
         //Assert
         CollectionAssert.AreEquivalent(newRecipes, expected_recipes);
     }
+    [TestMethod]
+    public void SearchRecipesByTagsTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: null,
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:null,
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByTags(tags);
+        //Assert
+        CollectionAssert.AreEquivalent(newRecipes, recipes);
+    }
+    [TestMethod]
+    public void SearchRecipesByTimeConstraintTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: null,
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:null,
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByTimeConstraint(5);
+        //Assert
+        Assert.AreEqual(newRecipes.Count, 1);
+    }
+    [TestMethod]
+    public void SearchRecipesByRatingTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: null,
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:null,
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByRating(2);
+        Assert.AreEqual(newRecipes.Count, 1);
+    }
+    [TestMethod]
+    public void SearchRecipesByServingsTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: null,
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:null,
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByServings(1);
+        //Assert
+        Assert.AreEqual(newRecipes.Count, 1);
+    }
+    [TestMethod]
+    public void SearchRecipesInFavoritesTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: null,
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:null,
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesInFavorites(0);
+        //Assert
+        Assert.AreEqual(newRecipes.Count, 1);
+    }
+    [TestMethod]
+    public void SearchRecipesByOwnerUsernameTest()
+    {
+        //Arrange
+        List<string> tags = new List<string>{"vegetarian","vegan"};
+        List<Recipe> recipes = new List<Recipe>{    
+            new Recipe(
+            name: "recipe1",
+            description: "description1",
+            cookingTime: 3,
+            instruction: "instruction1",
+            servings: 2,
+            ratings: 2,
+            ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
+            favourite: 2,
+            owner: new User("NotJoe","password","description", 20),
+            tags: tags),
+            new Recipe(
+            name: "recipe2",
+            description: "description2",
+            cookingTime: 0,
+            instruction: "Instruction 2",
+            servings: 1,
+            ratings: 3,
+            ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
+            favourite: 0,
+            owner:new User("Joe","password","description", 20),
+            tags: tags)};
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByOwnerUsername("Joe");
+        //Assert
+        Assert.AreEqual(newRecipes[0].Owner.Username, "Joe");
+    }
 }
