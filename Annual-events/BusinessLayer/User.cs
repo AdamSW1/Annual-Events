d8d75f321cd0932af575f205757d80013e2b0dcb
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-04-17 22:35:28 -0400

    Added a backing field for each of the variables

--- Annual-events/BusinessLayer/User.cs
+++ Annual-events/BusinessLayer/User.cs
@@ -1,61 +1,110 @@
 using RecipeInfo;
 namespace BusinessLayer;
 
-class User {
+class User
+{
 
-    private string username;
-	private string password;
-	private string description;
-	private int age;
-	private string ?profile_picture; // (GUI)
+    private string _username;
+    public string Username
+    {
+        get
+        {
+            return _username;
+        }
+        set
+        {
+            _username = value;
+        }
+    }
+    private string _password;
+    public string Password
+    {
+        get
+        {
+            return _password;
+        }
+        set
+        {
+            _password = value;
+        }
+    }
+    private string _description;
+    public string Description
+    {
+        get
+        {
+            return _description;
+        }
+        set
+        {
+            _description = value;
+        }
+    }
+    private int _age;
+    public int Age 
+    {
+        get 
+        {
+            return _age; 
+        }
+        set 
+        {
+            _age = value; 
+        }
+    }
+    private string? profile_picture; // (GUI)
 
     private List<Recipe> recipes = new List<Recipe>();
 
-    public string Username { get { return username; } }
-    public string Description { get { return description; } }
-    public int Age { get { return age; } }
-
     // constructor 
-    public User(string username, string password, string description, int age) {
-        this.username = username;
-        this.password = password;
-        this.description = description;
-        this.age = age;
+    public User(string username, string password, string description, int age)
+    {
+        _username = username;
+        _password = password;
+        _description = description;
+        _age = age;
         hashPassword(password);
     }
 
     public void DisplayUserInfo()
     {
-        Console.WriteLine($"Username: {username}");
-        Console.WriteLine($"Description: {description}");
-        Console.WriteLine($"Age: {age}");
+        Console.WriteLine($"Username: {_username}");
+        Console.WriteLine($"Description: {_description}");
+        Console.WriteLine($"Age: {_age}");
     }
-	public void AddToFavRecipe(Recipe recipe) {
+    public void AddToFavRecipe(Recipe recipe)
+    {
         recipes.Add(recipe);
     } // lets them add a recipe to an [] list
-	public void AddRecipe(Recipe recipe) {
+    public void AddRecipe(Recipe recipe)
+    {
         recipe.Owner = this;
         recipes.Add(recipe);
     } // just adding a recipe to a list, other than Fav
-	public void RemoveFromFavRecipe(Recipe recipe) {
+    public void RemoveFromFavRecipe(Recipe recipe)
+    {
         recipes.Remove(recipe);
     } // removes a recipe from the [] list
 
-    public void DisplayRecipes() {
-        Console.WriteLine($"Recipes for user: {username}\n");
+    public void DisplayRecipes()
+    {
+        Console.WriteLine($"Recipes for user: {_username}\n");
         foreach (var recipe in recipes)
         {
             recipe.DisplayRecipeInfo();
         }
     }
-	public void StepVerification() {} // optional but asks them 2 specific infos
-	public void hashPassword(string password) {
+    public void StepVerification() { } // optional but asks them 2 specific infos
+    public void hashPassword(string password)
+    {
         // algo here
     } // hashes their password
-	public bool Authentication(string enteredUsername, string enteredPassword) {
-        return username == enteredUsername && password == enteredPassword;
+    public bool Authentication(string enteredUsername, string enteredPassword)
+    {
+        return Username == enteredUsername && Password == enteredPassword;
     } // verifies their passwords and usernames with database
-	public void DeleteAccount() {
-        
+    public void DeleteAccount()
+    {
+
     } // should delete their account definitely
 }
