fa2827c507c9dba67a773ec227c59d61b765fb48
AdamSW1 <adamseanwinter@outlook.com>
2024-04-20 07:14:50 -0400

    reorganized class and added api link to summary

--- Annual-events/BusinessLayer/API/Ingredient_json.cs
+++ Annual-events/BusinessLayer/API/Ingredient_json.cs
@@ -5,22 +5,28 @@ namespace API;
 /// !Important! : field names MUST match the names in the api, case sensitive
 /// uncomment fields to access them from the api
 /// note: brand and category both cannot be accessed
+/// 
+/// Api json, must be using Dawson vm 
+/// <see cref="http://10.172.19.128:8080/"/>
 /// </summary>
 public class Ingredient_json
 {
     public string? name { get; set; }
+    public Dictionary<string, USDPrice>? price { get; set; }
+    public string? image { get; set; }
+
+
+    // extra data
     // public string? id { get; set; }
     // public string? country { get; set; }
     // public string? seasonal { get; set; }
-    // public string? Category { get; set; }
-    // public string? Brand {get; set;}
+    // public string? Category { get; set; } //cannot be accessed
+    // public string? Brand {get; set;} // cannot be accessed
     // public string? weight { get; set;}
     // public bool hot{ get; set; }
     // public bool cold{ get; set; }
     // public bool frozen{ get; set; }
     // public string? description { get; set; }
-    public Dictionary<string, USDPrice>? price { get; set; }
-    public string? image { get; set; }
     // public Dictionary<string, string>? nutrition { get; set; }
     // public string? ingredients { get; set; }
 
