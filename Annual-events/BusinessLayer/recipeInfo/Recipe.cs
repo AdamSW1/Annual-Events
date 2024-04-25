fc685e27fb90fb9649f914169d8f48dc6da9da30
AdamSW1 <adamseanwinter@outlook.com>
2024-04-24 22:48:57 -0400

    moved validation to recipe

--- Annual-events/BusinessLayer/recipeInfo/Recipe.cs
+++ Annual-events/BusinessLayer/recipeInfo/Recipe.cs
@@ -4,7 +4,7 @@ using Microsoft.VisualBasic;
 namespace RecipeInfo;
 public class Recipe
 {
-    public Utils Utils = new Utils();
+    public Utils Utils = new();
     private User _owner;
     public User Owner
     {
@@ -21,6 +21,10 @@ public class Recipe
         }
         set
         {
+            if (!Utils.CheckName(value))
+            {
+                throw new ArgumentException("Invalid name");
+            }
             _name = value;
         }
     }
@@ -33,9 +37,14 @@ public class Recipe
         }
         set
         {
+            if (value is null)
+            {
+                throw new ArgumentException("description cant be null");
+            }
             _description = value;
         }
     }
+
     private double _cookingTime;
     public double CookingTime
     {
@@ -45,13 +54,17 @@ public class Recipe
         }
         set
         {
+            if (!Utils.CheckDouble(value))
+            {
+                throw new ArgumentException("Cooking time cant be negative");
+            }
             _cookingTime = value;
         }
     }
 
     // make a list of preparation objects
-    private string _preparation;
-    public string Preparation
+    private List<string> _preparation;
+    public List<string> Preparation
     {
         get
         {
@@ -59,6 +72,10 @@ public class Recipe
         }
         set
         {
+            if (!Utils.CheckList(value))
+            {
+                throw new ArgumentException("Recipe must have a preparation");
+            }
             _preparation = value;
         }
     }
@@ -71,6 +88,10 @@ public class Recipe
         }
         set
         {
+            if (!Utils.CheckInt(value))
+            {
+                throw new ArgumentException("Servings must be greater than 0");
+            }
             _servings = value;
         }
     }
@@ -83,7 +104,7 @@ public class Recipe
         }
         set
         {
-            if (value > 5  || value < 1)
+            if (!Utils.CheckRatings(value))
             {
                 throw new ArgumentException("Rating should be between 1 and 5.");
             }
@@ -103,6 +124,9 @@ public class Recipe
         }
         set
         {
+            if(!Utils.CheckList(value)){
+                
+            }
             _ingredients = value;
         }
 
@@ -110,13 +134,7 @@ public class Recipe
 
     //make many to many
     private int _favourite;
-    public int Favourite
-    {
-        get
-        {
-            return _favourite;
-        }
-    }
+    public int Favourite { get; set; }
 
     private List<RecipeTags> _tags;
     public List<RecipeTags> Tags
@@ -131,15 +149,16 @@ public class Recipe
         }
     }
 
-    private List<Review> _reviews = new(){};
+    private List<Review> _reviews = new() { };
     public List<Review> Reviews
     {
-        get { 
-                _reviews ??= new List<Review>();
-                return _reviews; 
-            }
+        get
+        {
+            _reviews ??= new List<Review>();
+            return _reviews;
+        }
         set { _reviews = value; }
-    } 
+    }
 
     /// <summary>
     /// Constructor
@@ -156,9 +175,9 @@ public class Recipe
     /// <param name="tags"></param>
     public Recipe(
         string name, string description,
-        double cookingTime, string preparation, int servings, 
+        double cookingTime, List<string> preparation, int servings,
         double ratings, List<Ingredient> ingredients,
-        int favourite, User owner,List<string> tags, List<Review> reviews
+        int favourite, User owner, List<string> tags, List<Review> reviews
     )
     {
         _name = name;
@@ -177,19 +196,19 @@ public class Recipe
     // override object.Equals
     public override bool Equals(object obj)
     {
-        
+
         if (obj == null || GetType() != obj.GetType())
         {
             return false;
         }
-        
+
         Recipe other = (Recipe)obj;
         bool namesEqual = _name.Equals(other._name);
         bool descriptionsEqual = _description.Equals(other._description);
         bool ownersEqual = (_owner == null && other._owner == null) || (_owner != null && _owner.Equals(other._owner));
         return namesEqual && descriptionsEqual && ownersEqual;
     }
-    
+
     // override object.GetHashCode
     public override int GetHashCode()
     {
