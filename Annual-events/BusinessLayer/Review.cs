906aa8a10b99ad755991d9ad0c336d096e2e4088
AdamSW1 <adamseanwinter@outlook.com>
2024-04-25 16:45:00 -0400

    fixed missing brackets

--- Annual-events/BusinessLayer/Review.cs
+++ Annual-events/BusinessLayer/Review.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 using RecipeInfo;
 namespace BusinessLayer;
 
@@ -9,19 +10,20 @@ public class Review
     public int ReviewId { get; set; }
     private int _score;
     public int Score
-    { 
+    {
         get { return _score; }
     }
     private string _reviewerUsername;
-    public string ReviewerUsername 
-    { 
-        get { return _reviewerUsername; set _reviewerUsername = this.value } 
+    public string ReviewerUsername
+    {
+        get { return _reviewerUsername; }
+        set { _reviewerUsername = value; }
     }
 
     private string _reviewText;
-    public string ReviewText 
-    { 
-        get { return _reviewText; set _reviewText = value } 
+    public string ReviewText { 
+        get{ return _reviewText; } 
+        set{ _reviewText = value;}
     }
 
     public Review(string reviewerUsername, string reviewText, int score)
@@ -34,7 +36,7 @@ public class Review
     public override string ToString()
     {
         var str = new StringBuilder();
-        str.Append($"- review by ")
+        str.Append($"- review by ");
         str.Append($"{ReviewerUsername}");
         str.Append($"{ReviewText}.");
         str.Append($"{Score}/5");
