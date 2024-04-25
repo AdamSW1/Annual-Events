7453ecff3384424366b2005bb68766d8c3c565bf
AdamSW1 <adamseanwinter@outlook.com>
2024-04-25 03:26:41 -0400

    made method to create example recipe list

--- annual-events-test/Search-Test.cs
+++ annual-events-test/Search-Test.cs
@@ -16,31 +16,7 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         List<Recipe> expected_recipes = new List<Recipe> { recipes[0] };
         //Act
@@ -53,31 +29,7 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         //Act
         List<Recipe> newRecipes = search.SearchRecipesByTags(tags);
@@ -89,31 +41,7 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         //Act
         List<Recipe> newRecipes = search.SearchRecipesByTimeConstraint(5);
@@ -125,31 +53,7 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         //Act
         List<Recipe> newRecipes = search.SearchRecipesByRating(2);
@@ -160,31 +64,7 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};    
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         //Act
         List<Recipe> newRecipes = search.SearchRecipesByServings(1);
@@ -196,72 +76,57 @@ public class SearchTest
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List<Recipe> recipes = new List<Recipe>{
-            new Recipe(
-                name: "recipe1",
-                description: "description1",
-                cookingTime: 3,
-                preparation: "preparation1",
-                servings: 2,
-                ratings: 2,
-                ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
-                favourite: 2,
-                owner: null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
-            new Recipe(
-                name: "recipe2",
-                description: "description2",
-                cookingTime: 0,
-                preparation: "Preparation 2",
-                servings: 1,
-                ratings: 3,
-                ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
-                favourite: 0,
-                owner:null,
-                tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
+        List<Recipe> recipes = CreateExampleList();
         Search search = new Search(recipes);
         //Act
         List<Recipe> newRecipes = search.SearchRecipesInFavorites(0);
         //Assert
-        Assert.AreEqual(newRecipes.Count, 1);
+        Assert.AreEqual(newRecipes.Count, 2);
     }
     [TestMethod]
     public void SearchRecipesByOwnerUsernameTest()
     {
         //Arrange
         List<string> tags = new List<string> { "vegetarian", "vegan" };
-        List <string> review =  new List<string> { "review1", "review2" };
+        List<string> review = new List<string> { "review1", "review2" };
+        List<Recipe> recipes = CreateExampleList();
+        Search search = new Search(recipes);
+        //Act
+        List<Recipe> newRecipes = search.SearchRecipesByOwnerUsername("Joe");
+        //Assert
+        Assert.AreEqual(newRecipes[0].Owner.Username, "Joe");
+    }
+
+    private List<Recipe> CreateExampleList()
+    {
+        List<string> tags = new List<string> { "vegetarian", "vegan" };
+        List<string> review = new List<string> { "review1", "review2" };
         List<Recipe> recipes = new List<Recipe>{
             new Recipe(
                 name: "recipe1",
                 description: "description1",
                 cookingTime: 3,
-                preparation: "preparation1",
+                preparation: new List<string>(){"do"},
                 servings: 2,
                 ratings: 2,
                 ingredients: new List<Ingredient>{new Ingredient("cheese","2",3)},
                 favourite: 2,
                 owner: new User("Joe", "password", "Joe", 30),
                 tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1"), new Review("reviewer2", "review2") }),
+                reviews: new List<Review> { new("reviewer1", "review1",0), new Review("reviewer2", "review2",0) }),
             new Recipe(
                 name: "recipe2",
                 description: "description2",
                 cookingTime: 0,
-                preparation: "Preparation 2",
+                preparation: new List<string>(){"dont do"},
                 servings: 1,
                 ratings: 3,
                 ingredients: new List<Ingredient> {new Ingredient("carrot", "2", 3)},
                 favourite: 0,
                 owner:new User("Boe", "password", "Boe", 30),
                 tags: tags,
-                reviews: new List<Review> { new Review("reviewer1", "review1") })};
-        Search search = new Search(recipes);
-        //Act
-        List<Recipe> newRecipes = search.SearchRecipesByOwnerUsername("Joe");
-        //Assert
-        Assert.AreEqual(newRecipes[0].Owner.Username, "Joe");
+                reviews: new List<Review> { new("reviewer1", "review1",0) })};
+
+        return recipes;
     }
 }
