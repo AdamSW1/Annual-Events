2593c7bfa22d9e330b3896b451a9c72a3dd93597
AdamSW1 <adamseanwinter@outlook.com>
2024-05-02 15:53:16 -0400

    added get add and remove preparation

--- Annual-events/DataLayer/AnnualEventsServices.cs
+++ Annual-events/DataLayer/AnnualEventsServices.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using BusinessLayer;
 using RecipeInfo;
+
 namespace DataLayer;
 
 public class AnnualEventsService
@@ -10,6 +11,7 @@ public class AnnualEventsService
     public static AnnualEventsService Instance{
         get { return _instance ??= _instance = new AnnualEventsService();}
     }
+
     public AnnualEventsContext DbContext = AnnualEventsContext.Instance;
     public AnnualEventsService(){}
 
@@ -41,4 +43,27 @@ public class AnnualEventsService
 
     }
 
+    public Preparation GetPreparation(int id){
+        Preparation preparation = (Preparation)DbContext.Preparation
+                                    .Where(prep => prep.PreparationID == id)
+                                    .First();
+        
+        return preparation;
+    }
+    public void AddPreparation(Preparation preparation){
+        DbContext.Preparation.Add(preparation);
+        DbContext.SaveChanges();
+    }
+
+    internal void RemovePreparation(Preparation preparation)
+    {
+        var query = from Preparation in DbContext.Preparation
+        where preparation.PreparationID == preparation.PreparationID
+        select preparation;
+
+        foreach (var prep in query){
+            DbContext.Preparation.Remove(prep);
+        }
+
+    }
 }
