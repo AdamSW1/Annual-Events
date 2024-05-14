c1a5dadd9321c3afb7d28edbd540b28c9bded754
AdamSW1 <adamseanwinter@outlook.com>
2024-05-14 15:23:20 -0400

    changed back to oracle

--- Annual-events/Migrations/20240514191748_initialCreate.cs
+++ Annual-events/Migrations/20240514191748_initialCreate.cs
@@ -2,7 +2,7 @@
 
 #nullable disable
 
-namespace annualevents.Migrations
+namespace annual_events.Migrations
 {
     /// <inheritdoc />
     public partial class initialCreate : Migration
@@ -14,80 +14,79 @@ namespace annualevents.Migrations
                 name: "Annual_Events_User",
                 columns: table => new
                 {
-                    AnnualEventsUserId = table.Column<int>(name: "Annual_Events_UserId", type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Username = table.Column<string>(type: "TEXT", nullable: false),
-                    Password = table.Column<string>(type: "TEXT", nullable: false),
-                    Description = table.Column<string>(type: "TEXT", nullable: false),
-                    Age = table.Column<int>(type: "INTEGER", nullable: false)
+                    Annual_Events_UserId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Username = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Password = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Age = table.Column<int>(type: "NUMBER(10)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Annual_Events_User", x => x.AnnualEventsUserId);
+                    table.PrimaryKey("PK_Annual_Events_User", x => x.Annual_Events_UserId);
                 });
 
             migrationBuilder.CreateTable(
-                name: "Ingredient",
+                name: "Ingredients",
                 columns: table => new
                 {
-                    IngredientId = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Name = table.Column<string>(type: "TEXT", nullable: false),
-                    Price = table.Column<double>(type: "REAL", nullable: false)
+                    IngredientId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Name = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Price = table.Column<double>(type: "BINARY_DOUBLE", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Ingredient", x => x.IngredientId);
+                    table.PrimaryKey("PK_Ingredients", x => x.IngredientId);
                 });
 
             migrationBuilder.CreateTable(
                 name: "Preparation",
                 columns: table => new
                 {
-                    PreparationID = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    StepNumber = table.Column<int>(type: "INTEGER", nullable: false),
-                    Step = table.Column<string>(type: "TEXT", nullable: false)
+                    PreparationID = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    StepNumber = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Step = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Preparation", x => x.PreparationID);
-                    
                 });
 
             migrationBuilder.CreateTable(
-                name: "RecipeTag",
+                name: "RecipeTags",
                 columns: table => new
                 {
-                    RecipeTagId = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Tag = table.Column<string>(type: "TEXT", nullable: true)
+                    RecipeTagId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Tag = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_RecipeTag", x => x.RecipeTagId);
+                    table.PrimaryKey("PK_RecipeTags", x => x.RecipeTagId);
                 });
 
             migrationBuilder.CreateTable(
                 name: "Recipe",
                 columns: table => new
                 {
-                    RecipeID = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    OwnerAnnualEventsUserId = table.Column<int>(name: "OwnerAnnual_Events_UserId", type: "INTEGER", nullable: false),
-                    Name = table.Column<string>(type: "TEXT", nullable: false),
-                    Description = table.Column<string>(type: "TEXT", nullable: false),
-                    CookingTime = table.Column<double>(type: "REAL", nullable: false),
-                    Servings = table.Column<int>(type: "INTEGER", nullable: false),
-                    AverageScore = table.Column<double>(type: "REAL", nullable: false),
-                    Favourite = table.Column<int>(type: "INTEGER", nullable: false)
+                    RecipeID = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    OwnerAnnual_Events_UserId = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    Description = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    CookingTime = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Servings = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    AverageScore = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    Favourite = table.Column<int>(type: "NUMBER(10)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Recipe", x => x.RecipeID);
                     table.ForeignKey(
                         name: "FK_Recipe_Annual_Events_User_OwnerAnnual_Events_UserId",
-                        column: x => x.OwnerAnnualEventsUserId,
+                        column: x => x.OwnerAnnual_Events_UserId,
                         principalTable: "Annual_Events_User",
                         principalColumn: "Annual_Events_UserId",
                         onDelete: ReferentialAction.Cascade);
@@ -97,15 +96,15 @@ namespace annualevents.Migrations
                 name: "Annual_Events_UserRecipe",
                 columns: table => new
                 {
-                    FavRecipesRecipeID = table.Column<int>(type: "INTEGER", nullable: false),
-                    FavouritedByAnnualEventsUserId = table.Column<int>(name: "FavouritedByAnnual_Events_UserId", type: "INTEGER", nullable: false)
+                    FavRecipesRecipeID = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    FavouritedByAnnual_Events_UserId = table.Column<int>(type: "NUMBER(10)", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Annual_Events_UserRecipe", x => new { x.FavRecipesRecipeID, x.FavouritedByAnnualEventsUserId });
+                    table.PrimaryKey("PK_Annual_Events_UserRecipe", x => new { x.FavRecipesRecipeID, x.FavouritedByAnnual_Events_UserId });
                     table.ForeignKey(
                         name: "FK_Annual_Events_UserRecipe_Annual_Events_User_FavouritedByAnnual_Events_UserId",
-                        column: x => x.FavouritedByAnnualEventsUserId,
+                        column: x => x.FavouritedByAnnual_Events_UserId,
                         principalTable: "Annual_Events_User",
                         principalColumn: "Annual_Events_UserId",
                         onDelete: ReferentialAction.Cascade);
@@ -121,8 +120,8 @@ namespace annualevents.Migrations
                 name: "PreparationRecipe",
                 columns: table => new
                 {
-                    PreparationID = table.Column<int>(type: "INTEGER", nullable: false),
-                    RecipesRecipeID = table.Column<int>(type: "INTEGER", nullable: false)
+                    PreparationID = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    RecipesRecipeID = table.Column<int>(type: "NUMBER(10)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -145,43 +144,43 @@ namespace annualevents.Migrations
                 name: "RecipeIngredients",
                 columns: table => new
                 {
-                    RecipeIngredientId = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    RecipeID = table.Column<int>(type: "INTEGER", nullable: true),
-                    IngredientId = table.Column<int>(type: "INTEGER", nullable: true),
-                    Quantity = table.Column<string>(type: "TEXT", nullable: true)
+                    RecipeIngredientId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    RecipeID = table.Column<int>(type: "NUMBER(10)", nullable: true),
+                    IngredientId = table.Column<int>(type: "NUMBER(10)", nullable: true),
+                    Quantity = table.Column<string>(type: "NVARCHAR2(2000)", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_RecipeIngredients", x => x.RecipeIngredientId);
                     table.ForeignKey(
-                        name: "FK_RecipeIngredients_Ingredient_IngredientId",
+                        name: "FK_RecipeIngredients_Ingredients_IngredientId",
                         column: x => x.IngredientId,
-                        principalTable: "Ingredient",
-                        principalColumn: "IngredientId",
-                        onDelete: ReferentialAction.Cascade);
+                        principalTable: "Ingredients",
+                        principalColumn: "IngredientId");
                     table.ForeignKey(
                         name: "FK_RecipeIngredients_Recipe_RecipeID",
                         column: x => x.RecipeID,
                         principalTable: "Recipe",
                         principalColumn: "RecipeID",
                         onDelete: ReferentialAction.Cascade);
+
                 });
 
             migrationBuilder.CreateTable(
                 name: "RecipeRecipeTag",
                 columns: table => new
                 {
-                    RecipeWithTagsRecipeID = table.Column<int>(type: "INTEGER", nullable: false),
-                    TagsRecipeTagId = table.Column<int>(type: "INTEGER", nullable: false)
+                    RecipeWithTagsRecipeID = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    TagsRecipeTagId = table.Column<int>(type: "NUMBER(10)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_RecipeRecipeTag", x => new { x.RecipeWithTagsRecipeID, x.TagsRecipeTagId });
                     table.ForeignKey(
-                        name: "FK_RecipeRecipeTag_RecipeTag_TagsRecipeTagId",
+                        name: "FK_RecipeRecipeTag_RecipeTags_TagsRecipeTagId",
                         column: x => x.TagsRecipeTagId,
-                        principalTable: "RecipeTag",
+                        principalTable: "RecipeTags",
                         principalColumn: "RecipeTagId",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
@@ -196,12 +195,12 @@ namespace annualevents.Migrations
                 name: "Review",
                 columns: table => new
                 {
-                    ReviewId = table.Column<int>(type: "INTEGER", nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    RecipeID = table.Column<int>(type: "INTEGER", nullable: false),
-                    Score = table.Column<int>(type: "INTEGER", nullable: false),
-                    ReviewerUsername = table.Column<string>(type: "TEXT", nullable: false),
-                    ReviewText = table.Column<string>(type: "TEXT", nullable: false)
+                    ReviewId = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    RecipeID = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    Score = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    ReviewerUsername = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    ReviewText = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -272,10 +271,10 @@ namespace annualevents.Migrations
                 name: "Preparation");
 
             migrationBuilder.DropTable(
-                name: "Ingredient");
+                name: "Ingredients");
 
             migrationBuilder.DropTable(
-                name: "RecipeTag");
+                name: "RecipeTags");
 
             migrationBuilder.DropTable(
                 name: "Recipe");
