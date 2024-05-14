0547a82eb9d3ac8cf021679fc228c95b14fc9635
AdamSW1 <adamseanwinter@outlook.com>
2024-05-14 17:43:18 -0400

    logged in view and view model

---
+++ MalfunctioningKitchen/Views/LoggedInView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MalfunctioningKitchen.Views;
+
+public partial class LoggedInView : UserControl
+{
+    public LoggedInView()
+    {
+        InitializeComponent();
+    }
+}
