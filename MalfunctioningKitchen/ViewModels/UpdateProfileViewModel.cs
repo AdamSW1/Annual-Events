39998a3614c917684ca118241cacd7eb37a666c9
Adam Winter <adamseanwinter@outlook.com>
2024-05-19 03:58:13 -0400

    added current values to text boxes and labels

--- MalfunctioningKitchen/ViewModels/UpdateProfileViewModel.cs
+++ MalfunctioningKitchen/ViewModels/UpdateProfileViewModel.cs
@@ -8,10 +8,10 @@ namespace MalfunctioningKitchen.ViewModels
 {
     public class UpdateProfileViewModel : ViewModelBase
     {
-        private string _username;
-        private string _password;
-        private string _description;
-        private int _age;
+        private string _username = AuthenticationManager.Instance.CurrentUser.Username!;
+        private string _password = "**********";
+        private string _description = AuthenticationManager.Instance.CurrentUser.Description!;
+        private int _age = AuthenticationManager.Instance.CurrentUser.Age;
 
         private Annual_Events_User _user;
         public Annual_Events_User User
