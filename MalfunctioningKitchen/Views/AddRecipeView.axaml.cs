f2a83cb13d847187641f0b945183c07ce53f74a6
Vijay Patel <vijay.patel@dawsoncollege.qc.ca>
2024-05-19 23:44:59 -0400

    Created the AddRecipeViiew

---
+++ MalfunctioningKitchen/Views/AddRecipeView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace MalfunctioningKitchen.Views;
+
+public partial class AddRecipeView : UserControl
+{
+    public AddRecipeView()
+    {
+        InitializeComponent();
+    }
+}
