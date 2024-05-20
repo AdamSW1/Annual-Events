cb7ba3df99dc1025d4d0ac4e50b35679c7a5869a
TonyDoReMi <117762250+TonyDoReMi@users.noreply.github.com>
2024-05-20 12:06:25 -0400

    Formatted the files

--- Annual-events/BusinessLayer/Search.cs
+++ Annual-events/BusinessLayer/Search.cs
@@ -4,122 +4,122 @@ using RecipeInfo;
 using DataLayer;
 using System.Linq;
 
-namespace BusinessLayer
+namespace BusinessLayer;
+
+public class Search
 {
-    public class Search
+    public static List<Recipe> GetRecipes()
     {
-        public static List<Recipe> GetRecipes()
+        using (var dbContext = new AnnualEventsContext())
         {
-            using (var dbContext = new AnnualEventsContext())
-            {
-                var recipes = dbContext.Recipe.ToList();
-                Console.WriteLine($"Retrieved {recipes.Count} recipes from the database.");
-                return recipes;
-            }
-        }
-
-        // Search recipes by keyword
-        public static List<Recipe> SearchRecipesByKeyword(string keyword, List<Recipe> recipes)
-        {
-            string escaped = Regex.Escape(keyword);
-            var reg = new Regex(escaped, RegexOptions.IgnoreCase);
-            var searched = recipes.Where(recipe => reg.IsMatch(recipe.Name) || reg.IsMatch(recipe.Description) || recipe.Preparation.Any(x => reg.IsMatch(x.Step))).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by keyword: {keyword}");
-            return searched;
-        }
-
-        // Search recipes by tags
-        public static List<Recipe>? SearchRecipesByTags(List<RecipeTag> tags, List<Recipe> recipes)
-        {
-            if (!Utils.ValidateTags(tags))
-            {
-                return null;
-            }
-            var searched = recipes.Where(recipe => recipe.Tags.Intersect(tags).Any()).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by tags: {string.Join(", ", tags.Select(tag => tag.Tag))}");
-            return searched;
-        }
-
-        public static List<Recipe> SearchRecipesByTimeConstraint(int time, List<Recipe> recipes)
-        {
-            var searched = recipes.Where(recipe => recipe.CookingTime >= time - 3 && recipe.CookingTime <= time + 3).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by time constraint: {time}");
-            return searched;
-        }
-
-        // Search recipes by rating
-        public static List<Recipe> SearchRecipesByRating(int rating, List<Recipe> recipes)
-        {
-            var searched = recipes.Where(recipe => recipe.AverageScore == rating).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by rating: {rating}");
-            return searched;
-        }
-
-        // Search recipes by servings constraint
-        public static List<Recipe> SearchRecipesByServings(int servings, List<Recipe> recipes)
-        {
-            var searched = recipes.Where(recipe => recipe.Servings == servings).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by servings: {servings}");
-            return searched;
-        }
-
-        // Search recipes in favorites
-        public static List<Recipe> SearchRecipesInFavorites(int favourite, List<Recipe> recipes)
-        {
-            var searched = recipes.Where(recipe => recipe.Favourite == favourite).ToList();
-            Console.WriteLine($"Found {searched.Count} recipes by favorite: {favourite}");
-            return searched;
-        }
-
-        // Search recipes by owner username
-        public static List<Recipe> SearchRecipesByOwnerUsername(string ownerUsername, List<Recipe> recipes)
-        {
-            var searched = recipes.Where(recipe => recipe.Owner.Username != null && recipe.Owner.Username.Equals(ownerUsername, StringComparison.OrdinalIgnoreCase));
-            return searched.ToList();
-        }
-
-        // Example of chaining search queries
-        public static List<Recipe> SearchRecipes(string keyword = null, List<RecipeTag> tags = null, int? time = null, int? rating = null, int? servings = null, int? favourite = null, string ownerUsername = null)
-        {
-            List<Recipe> searchedRecipes = GetRecipes();
-
-            if (!string.IsNullOrEmpty(keyword))
-            {
-                searchedRecipes = SearchRecipesByKeyword(keyword, searchedRecipes);
-            }
-
-            if (tags != null && tags.Any())
-            {
-                searchedRecipes = SearchRecipesByTags(tags, searchedRecipes);
-            }
-
-            if (time.HasValue)
-            {
-                searchedRecipes = SearchRecipesByTimeConstraint(time.Value, searchedRecipes);
-            }
-
-            if (rating.HasValue)
-            {
-                searchedRecipes = SearchRecipesByRating(rating.Value, searchedRecipes);
-            }
-
-            if (servings.HasValue)
-            {
-                searchedRecipes = SearchRecipesByServings(servings.Value, searchedRecipes);
-            }
-
-            if (favourite.HasValue)
-            {
-                searchedRecipes = SearchRecipesInFavorites(favourite.Value, searchedRecipes);
-            }
-
-            if (!string.IsNullOrEmpty(ownerUsername))
-            {
-                searchedRecipes = SearchRecipesByOwnerUsername(ownerUsername, searchedRecipes);
-            }
-
-            Console.WriteLine($"Total recipes found: {searchedRecipes.Count}");
-            return searchedRecipes;
+            var recipes = dbContext.Recipe.ToList();
+            Console.WriteLine($"Retrieved {recipes.Count} recipes from the database.");
+            return recipes;
         }
     }
+
+    // Search recipes by keyword
+    public static List<Recipe> SearchRecipesByKeyword(string keyword, List<Recipe> recipes)
+    {
+        string escaped = Regex.Escape(keyword);
+        var reg = new Regex(escaped, RegexOptions.IgnoreCase);
+        var searched = recipes.Where(recipe => reg.IsMatch(recipe.Name) || reg.IsMatch(recipe.Description) || recipe.Preparation.Any(x => reg.IsMatch(x.Step))).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by keyword: {keyword}");
+        return searched;
+    }
+
+    // Search recipes by tags
+    public static List<Recipe>? SearchRecipesByTags(List<RecipeTag> tags, List<Recipe> recipes)
+    {
+        if (!Utils.ValidateTags(tags))
+        {
+            return null;
+        }
+        var searched = recipes.Where(recipe => recipe.Tags.Intersect(tags).Any()).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by tags: {string.Join(", ", tags.Select(tag => tag.Tag))}");
+        return searched;
+    }
+
+    public static List<Recipe> SearchRecipesByTimeConstraint(int time, List<Recipe> recipes)
+    {
+        var searched = recipes.Where(recipe => recipe.CookingTime >= time - 3 && recipe.CookingTime <= time + 3).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by time constraint: {time}");
+        return searched;
+    }
+
+    // Search recipes by rating
+    public static List<Recipe> SearchRecipesByRating(int rating, List<Recipe> recipes)
+    {
+        var searched = recipes.Where(recipe => recipe.AverageScore == rating).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by rating: {rating}");
+        return searched;
+    }
+
+    // Search recipes by servings constraint
+    public static List<Recipe> SearchRecipesByServings(int servings, List<Recipe> recipes)
+    {
+        var searched = recipes.Where(recipe => recipe.Servings == servings).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by servings: {servings}");
+        return searched;
+    }
+
+    // Search recipes in favorites
+    public static List<Recipe> SearchRecipesInFavorites(int favourite, List<Recipe> recipes)
+    {
+        var searched = recipes.Where(recipe => recipe.Favourite == favourite).ToList();
+        Console.WriteLine($"Found {searched.Count} recipes by favorite: {favourite}");
+        return searched;
+    }
+
+    // Search recipes by owner username
+    public static List<Recipe> SearchRecipesByOwnerUsername(string ownerUsername, List<Recipe> recipes)
+    {
+        var searched = recipes.Where(recipe => recipe.Owner.Username != null && recipe.Owner.Username.Equals(ownerUsername, StringComparison.OrdinalIgnoreCase));
+        return searched.ToList();
+    }
+
+    // Example of chaining search queries
+    public static List<Recipe> SearchRecipes(string keyword = null, List<RecipeTag> tags = null, int? time = null, int? rating = null, int? servings = null, int? favourite = null, string ownerUsername = null)
+    {
+        List<Recipe> searchedRecipes = GetRecipes();
+
+        if (!string.IsNullOrEmpty(keyword))
+        {
+            searchedRecipes = SearchRecipesByKeyword(keyword, searchedRecipes);
+        }
+
+        if (tags != null && tags.Any())
+        {
+            searchedRecipes = SearchRecipesByTags(tags, searchedRecipes);
+        }
+
+        if (time.HasValue)
+        {
+            searchedRecipes = SearchRecipesByTimeConstraint(time.Value, searchedRecipes);
+        }
+
+        if (rating.HasValue)
+        {
+            searchedRecipes = SearchRecipesByRating(rating.Value, searchedRecipes);
+        }
+
+        if (servings.HasValue)
+        {
+            searchedRecipes = SearchRecipesByServings(servings.Value, searchedRecipes);
+        }
+
+        if (favourite.HasValue)
+        {
+            searchedRecipes = SearchRecipesInFavorites(favourite.Value, searchedRecipes);
+        }
+
+        if (!string.IsNullOrEmpty(ownerUsername))
+        {
+            searchedRecipes = SearchRecipesByOwnerUsername(ownerUsername, searchedRecipes);
+        }
+
+        Console.WriteLine($"Total recipes found: {searchedRecipes.Count}");
+        return searchedRecipes;
+    }
 }
+
--- MalfunctioningKitchen/ViewModels/SearchRecipeViewModel.cs
+++ MalfunctioningKitchen/ViewModels/SearchRecipeViewModel.cs
@@ -8,132 +8,132 @@ using System.Linq;
 using System;
 using System.Reactive.Linq;
 
-namespace MalfunctioningKitchen.ViewModels
+namespace MalfunctioningKitchen.ViewModels;
+
+public class SearchRecipeViewModel : ViewModelBase
 {
-    public class SearchRecipeViewModel : ViewModelBase
+    private string _notificationMessage;
+    private string _searchKeyword;
+    private List<string> _tagCriteria;
+    private string _timeConstraint;
+    private string _rating;
+    private string _servings;
+    private string _favorite;
+    private string _ownerUsername;
+    private List<Recipe> _searchedRecipes;
+
+    public string NotificationMessage
     {
-        private string _notificationMessage;
-        private string _searchKeyword;
-        private List<string> _tagCriteria;
-        private string _timeConstraint;
-        private string _rating;
-        private string _servings;
-        private string _favorite;
-        private string _ownerUsername;
-        private List<Recipe> _searchedRecipes;
+        get => _notificationMessage;
+        set => this.RaiseAndSetIfChanged(ref _notificationMessage, value);
+    }
 
-        public string NotificationMessage
+    public List<Recipe> SearchedRecipes
+    {
+        get => _searchedRecipes;
+        set => this.RaiseAndSetIfChanged(ref _searchedRecipes, value);
+    }
+
+    public string SearchKeyword
+    {
+        get => _searchKeyword;
+        set => this.RaiseAndSetIfChanged(ref _searchKeyword, value);
+    }
+
+    public List<string> TagCriteria
+    {
+        get => _tagCriteria;
+        set => this.RaiseAndSetIfChanged(ref _tagCriteria, value);
+    }
+
+    public string TimeConstraint
+    {
+        get => _timeConstraint;
+        set => this.RaiseAndSetIfChanged(ref _timeConstraint, value);
+    }
+
+    public string Rating
+    {
+        get => _rating;
+        set => this.RaiseAndSetIfChanged(ref _rating, value);
+    }
+
+    public string Servings
+    {
+        get => _servings;
+        set => this.RaiseAndSetIfChanged(ref _servings, value);
+    }
+
+    public string Favorite
+    {
+        get => _favorite;
+        set => this.RaiseAndSetIfChanged(ref _favorite, value);
+    }
+
+    public string OwnerUsername
+    {
+        get => _ownerUsername;
+        set => this.RaiseAndSetIfChanged(ref _ownerUsername, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> Return { get; }
+    public ReactiveCommand<Unit, List<Recipe>> SearchCommand { get; }
+
+    public SearchRecipeViewModel()
+    {
+        Return = ReactiveCommand.Create(() =>
         {
-            get => _notificationMessage;
-            set => this.RaiseAndSetIfChanged(ref _notificationMessage, value);
-        }
+            // Implementation for Return command
+        });
 
-        public List<Recipe> SearchedRecipes
+        SearchCommand = ReactiveCommand.CreateFromTask(ExecuteSearch);
+        _tagCriteria = new List<string>();
+        _searchedRecipes = new List<Recipe>();
+    }
+
+    private async Task<List<Recipe>> ExecuteSearch()
+    {
+        try
         {
-            get => _searchedRecipes;
-            set => this.RaiseAndSetIfChanged(ref _searchedRecipes, value);
-        }
+            List<RecipeTag> tags = _tagCriteria.Select(tag => new RecipeTag(tag)).ToList();
 
-        public string SearchKeyword
-        {
-            get => _searchKeyword;
-            set => this.RaiseAndSetIfChanged(ref _searchKeyword, value);
-        }
+            var timeConstraint = int.TryParse(TimeConstraint, out var time) ? time : (int?)null;
+            var rating = int.TryParse(Rating, out var rate) ? rate : (int?)null;
+            var servings = int.TryParse(Servings, out var serve) ? serve : (int?)null;
+            var favorite = int.TryParse(Favorite, out var fav) ? fav : (int?)null;
 
-        public List<string> TagCriteria
-        {
-            get => _tagCriteria;
-            set => this.RaiseAndSetIfChanged(ref _tagCriteria, value);
-        }
+            var searchedRecipes = Search.SearchRecipes(
+                keyword: SearchKeyword,
+                tags: tags,
+                time: timeConstraint,
+                rating: rating,
+                servings: servings,
+                favourite: favorite,
+                ownerUsername: OwnerUsername
+            );
 
-        public string TimeConstraint
-        {
-            get => _timeConstraint;
-            set => this.RaiseAndSetIfChanged(ref _timeConstraint, value);
-        }
-
-        public string Rating
-        {
-            get => _rating;
-            set => this.RaiseAndSetIfChanged(ref _rating, value);
-        }
-
-        public string Servings
-        {
-            get => _servings;
-            set => this.RaiseAndSetIfChanged(ref _servings, value);
-        }
-
-        public string Favorite
-        {
-            get => _favorite;
-            set => this.RaiseAndSetIfChanged(ref _favorite, value);
-        }
-
-        public string OwnerUsername
-        {
-            get => _ownerUsername;
-            set => this.RaiseAndSetIfChanged(ref _ownerUsername, value);
-        }
-
-        public ReactiveCommand<Unit, Unit> Return { get; }
-        public ReactiveCommand<Unit, List<Recipe>> SearchCommand { get; }
-
-        public SearchRecipeViewModel()
-        {
-            Return = ReactiveCommand.Create(() =>
+            if (searchedRecipes == null || !searchedRecipes.Any())
             {
-                // Implementation for Return command
-            });
-
-            SearchCommand = ReactiveCommand.CreateFromTask(ExecuteSearch);
-            _tagCriteria = new List<string>();
-            _searchedRecipes = new List<Recipe>();
-        }
-
-        private async Task<List<Recipe>> ExecuteSearch()
-        {
-            try
-            {
-                List<RecipeTag> tags = _tagCriteria.Select(tag => new RecipeTag(tag)).ToList();
-
-                var timeConstraint = int.TryParse(TimeConstraint, out var time) ? time : (int?)null;
-                var rating = int.TryParse(Rating, out var rate) ? rate : (int?)null;
-                var servings = int.TryParse(Servings, out var serve) ? serve : (int?)null;
-                var favorite = int.TryParse(Favorite, out var fav) ? fav : (int?)null;
-
-                var searchedRecipes = Search.SearchRecipes(
-                    keyword: SearchKeyword,
-                    tags: tags,
-                    time: timeConstraint,
-                    rating: rating,
-                    servings: servings,
-                    favourite: favorite,
-                    ownerUsername: OwnerUsername
-                );
-
-                if (searchedRecipes == null || !searchedRecipes.Any())
-                {
-                    NotificationMessage = "No recipes found matching the criteria.";
-                    var allRecipes = Search.GetRecipes();
-                    var ownerUsernames = allRecipes.Select(r => r.Owner?.Username).Where(username => username != null).ToList();
-                    NotificationMessage += $"\nInput owner username: {OwnerUsername}";
-                    NotificationMessage += $"\nAvailable owner usernames: {string.Join(", ", ownerUsernames)}";
-                }
-                else
-                {
-                    SearchedRecipes = searchedRecipes;
-                    NotificationMessage = $"{searchedRecipes.Count} recipes found.";
-                }
-
-                // SearchedRecipes = searchedRecipes;
+                NotificationMessage = "No recipes found matching the criteria.";
+                var allRecipes = Search.GetRecipes();
+                var ownerUsernames = allRecipes.Select(r => r.Owner?.Username).Where(username => username != null).ToList();
+                NotificationMessage += $"\nInput owner username: {OwnerUsername}";
+                NotificationMessage += $"\nAvailable owner usernames: {string.Join(", ", ownerUsernames)}";
             }
-            catch (Exception ex)
+            else
             {
-                NotificationMessage = $"Error during search: {ex.Message}";
+                SearchedRecipes = searchedRecipes;
+                NotificationMessage = $"{searchedRecipes.Count} recipes found.";
             }
 
-            return SearchedRecipes;
+            // SearchedRecipes = searchedRecipes;
         }
+        catch (Exception ex)
+        {
+            NotificationMessage = $"Error during search: {ex.Message}";
+        }
+
+        return SearchedRecipes;
     }
 }
+
