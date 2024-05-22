d8a1a52f40d6e4ae68c2c2a1ba9d478dc2202423
AdamSW1 <adamseanwinter@outlook.com>
2024-05-22 10:44:54 -0400

    fixed adding review back button

--- MalfunctioningKitchen/ViewModels/MainWindowViewModel.cs
+++ MalfunctioningKitchen/ViewModels/MainWindowViewModel.cs
@@ -18,6 +18,7 @@ namespace MalfunctioningKitchen.ViewModels
             private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
         }
         public delegate void previous();
+        public delegate void previousRecipe(Recipe recipe, previous prev);
 
         public ViewModelBase previousPage = new();
 
@@ -129,14 +130,14 @@ namespace MalfunctioningKitchen.ViewModels
             
             viewModel.NavigateToHomePageCommand.Subscribe(_ => previous());
             viewModel.Logout.Subscribe(_ => NavigateToWelcome());
-            viewModel.NavigateToAddReviewCommand.Subscribe(_ => NavigateToAddReview(recipe, previousPage));
+            viewModel.NavigateToAddReviewCommand.Subscribe(_ => NavigateToAddReview(recipe, previous));
             ContentViewModel = viewModel;
         }
 
-        public void NavigateToAddReview(Recipe recipe, previous previousPage) 
+        public void NavigateToAddReview(Recipe recipe, previous previous) 
         {
             AddReviewViewModel viewModel = new(recipe);
-            viewModel.NavigateToHomePageCommand.Subscribe(_ => previousPage());
+            viewModel.Back.Subscribe(_ => NavigateToRecipe(recipe,previous));
             ContentViewModel = viewModel;
         }
         
